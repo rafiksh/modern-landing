3c513e340e24f216d96b153148d2d818edc818ec
Rafik <rafikshmoury@gmail.com>
2021-09-18 23:25:41 +0300

    refactor: i18n setup

--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,15 @@
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
 
 import en from "./locales/en";
 import ar from "./locales/ar";
 
 i18n
+  .use(Backend)
   .use(LanguageDetector)
-  .use(initReactI18next)
+  .use(initReactI18next) // bind react-i18next to the instance
   .init({
     resources: {
       en: en,
@@ -16,15 +18,9 @@ i18n
     fallbackLng: "en",
     debug: false,
 
-    keySeparator: false, // we use content as keys
-
     interpolation: {
       escapeValue: false, // not needed for react!!
       formatSeparator: ",",
-    },
-
-    react: {
-      wait: true,
     },
   });
 
